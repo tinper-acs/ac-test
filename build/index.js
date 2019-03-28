99f76fdbf437f227a6d27da76dc0e4741b82c132
“jonyshi” <jonyshi@126.com>
2019-03-28 10:12:35 +0800

    publish 0.0.10

---
+++ build/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Test = require('./Test');
+
+var _Test2 = _interopRequireDefault(_Test);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+exports["default"] = _Test2["default"];
+module.exports = exports['default'];
